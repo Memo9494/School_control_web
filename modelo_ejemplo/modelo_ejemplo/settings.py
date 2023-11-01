ca743b78e6d05f3149fcfa6fa3e85eb2f8595b1a
Memo9494 <104597078+Memo9494@users.noreply.github.com>
2023-11-01 09:37:05 -0600

    asistencia por staff

--- modelo_ejemplo/modelo_ejemplo/settings.py
+++ modelo_ejemplo/modelo_ejemplo/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'classes.CustomUser'
 CRISPY_TEMPLATE_PACK = 'bootstrap4' # new
-
+LOGIN_REDIRECT_URL = 'home' # new
